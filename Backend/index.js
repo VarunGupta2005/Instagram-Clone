231a90a3da51988eb871441caf214363ab6d4728
VarunGupta2005 <varun03072005@gmail.com>
2025-06-27 05:03:59 +0530

    5th try

--- Backend/index.js
+++ Backend/index.js
@@ -30,9 +30,9 @@ app.use("/userPost", PostRoutes);
 app.use((err, req, res, next) => {
   res.send("Oki");
 });
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/Frontend/dist/index.html"));
 });
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
